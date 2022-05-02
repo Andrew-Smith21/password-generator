bc79b8dc47c2181a6fc810181f49937982a8e8c4
Chad Smith <chad.smith@vanderbilt.edu>
2022-05-02 10:13:57 -0400

    setting up js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // Assignment code here
 
 
-// Get references to the #generate element
+function passwordLength() {
+
+  var pwLength = window.prompt("How many characters long should the password be? Enter a number from 8 to 128.");
+
+}
+
+
+
+
+// Starter Code Below
+
+// Get references to the #generate element / Tells computer where the button is
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  window.prompt("Test");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -13,5 +26,9 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Run writePassword function when the button is clicked
+generateBtn.addEventListener("click", writePassword());
+
+
+
+
